52b9e202e509c1ab4bd7f9c23cc4dcacde28b0cd
Lívia Araujo <liviaaraujo.dev@gmail.com>
2025-12-14 15:33:54 -0300

    feat: created CertificateRepository

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,32 @@
 <x-app-layout>
     <div class="flex justify-center flex-col items-center">
-           <div class="flex justify-center flex-col items-center my-10">
-        <h3 class="text-center font-bold text-2xl text-[#020817]">Certificados gerados</h3>
-        <span class="text-[#64748B]">Visualize todos os certificados gerados</span>
-    </div>
-
-    <div class="flex justify-center flex-col items-center border border-[#E2E8F0] w-[50%] py-20 gap-4 rounded-md">
-        <i class="ph ph-certificate text-[#64748B] text-6xl"></i>
-        <div class="flex flex-col items-center">
-            <span class="text-[#1A2856] font-bold">Nenhum certificado gerado</span>
-        <span class="text-[#64748B]">Gere certificados para consultar o histórico</span>
+       <div class="flex justify-center flex-col items-center my-10">
+            <h3 class="text-center font-bold text-2xl text-[#020817]">Certificados gerados</h3>
+            <span class="text-[#64748B]">Visualize todos os certificados gerados</span>
         </div>
-        <button class="bg-[#1A2856] text-white font-semibold px-4 rounded-md h-10">Gerar certificado</button>
-    </div>
+
+        <div class="flex justify-center flex-col items-center border border-[#E2E8F0] w-[50%] py-20 gap-4 rounded-md">
+            <i class="ph ph-certificate text-[#64748B] text-6xl"></i>
+            <div class="flex flex-col items-center">
+                <span class="text-[#1A2856] font-bold">Nenhum certificado gerado</span>
+            <span class="text-[#64748B]">Gere certificados para consultar o histórico</span>
+            </div>
+            <button class="bg-[#1A2856] text-white font-semibold px-4 rounded-md h-10">Gerar certificado</button>
+        </div>
+
+        <div class="w-full lg:w-[60%] xl:w-[45%] flex flex-col justify-cernter items-center">
+            @foreach($cetificates as $cetificate)
+                <div class="flex flex-col items-center justify-center mb-2 w-full">
+                    <div class="flex border border-[#E2E8F0] p-4 gap-4 rounded-md w-full ">
+                        <div class="flex flex-col w-full">
+{{--                            <span class="text-[#020817] font-bold text-base">{{$student->name}}</span>--}}
+                            <span class="text-[#64748B]">{{$cetificate->id}}</span>
+                        </div>
+                        <button type="submit"
+                                class="bg-gradient-to-tr from-[#1A2856] to-[#334EA9] px-4 h-10 rounded-md text-white font-semibold text-base">Ver</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 </x-app-layout>
