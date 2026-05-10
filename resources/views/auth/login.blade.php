f7420fa263161156fe1349430707b968d44d4553
Lívia Araujo <liviaaraujo.dev@gmail.com>
2026-05-10 19:06:00 -0300

    FIX: Style Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,11 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
+       <div class="flex justify-center">
+            <a href="/"> 
+             <img src="{{ asset('logo.svg') }}" alt="" class="h-16">
+            </a>
+       </div>
         @csrf
 
         <!-- Email Address -->
@@ -14,7 +19,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Senha')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -28,20 +33,14 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600">{{ __('Lembre de mim') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <button type="submit"  class="bg-gradient-to-tr from-[#1A2856] to-[#334EA9] h-12 rounded-md px-4 text-white font-semibold text-base flex items-center justify-center w-full">
+                Entrar
+            </button>
         </div>
     </form>
 </x-guest-layout>
